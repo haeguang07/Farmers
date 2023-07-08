ec56bbf1407ce1542295bd3396bdf8970aad3469
KSW <KSW@LAPTOP-BFJI6A4E>
2023-07-08 14:59:31 +0900

    0708 펀딩 등록 폼

---
+++ Farmers/src/main/java/com/yedam/app/market/vo/PageVO.java
@@ -0,0 +1,27 @@
+package com.yedam.app.market.vo;
+
+import lombok.Data;
+
+@Data
+public class PageVO {
+	private int startPage;
+	private int endPage;
+	private boolean prev;
+	private boolean next; 
+	private int pageNum;
+	
+	public PageVO(int pageNum, int total) {
+		this.pageNum = pageNum;
+		
+		this.endPage = (int)Math.ceil(this.pageNum/10.0)*10; //ceil 소수점올림
+		this.startPage = this.endPage-9;
+		
+		int realEnd = (int)(Math.ceil(total/10.0)); //아예 마지막 페이지
+		if(realEnd < this.endPage) {
+			this.endPage = realEnd;
+		}
+		this.prev = this.startPage>1;
+		this.next = this.endPage < realEnd; 
+	}
+	
+}
