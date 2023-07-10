db62b7ec495d4035c282875bc2ac0c5260e0ee91
user <user@DESKTOP-LGGFQD6>
2023-07-10 17:52:40 +0900

    7/10 희영 yedam

--- Farmers/src/main/java/com/yedam/app/market/controller/AuctionController.java
+++ Farmers/src/main/java/com/yedam/app/market/controller/AuctionController.java
@@ -1,16 +1,44 @@
 package com.yedam.app.market.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.yedam.app.market.service.AuctionService;
+import com.yedam.app.market.vo.AuctionVO;
 
 @Controller
 public class AuctionController {
 	
-	// 경매장 페이지 이동
+	@Autowired
+	AuctionService actService;
+	
+	// 전체조회 페이지
 	@GetMapping("auction")
-	public String auction() {
-		return "market/auction";
+	public String auctionList(Model model) {
+		List<AuctionVO> list = actService.getAllAuctions();
+		model.addAttribute("auction", list);
+		// System.out.println(list);
+		return "market/auction/auctionList";
 	}
 	
+	// 단건조회 페이지
+	@GetMapping("auctionInfo")
+	public String auctionInfo(int actNo, Model model) {
+		System.out.println(actNo);
+		AuctionVO info = actService.getOneAuction(actNo);
+		model.addAttribute("auctionInfo", info);
+		return "market/auction/auctionInfo";
+		
+	}
+	
+	// 등록 페이지
+	
+	// 등록 처리
+	
+	// 수정 페이지
 	
 }
--- Farmers/src/main/java/com/yedam/app/market/service/AuctionService.java
+++ Farmers/src/main/java/com/yedam/app/market/service/AuctionService.java
@@ -1,5 +1,22 @@
 package com.yedam.app.market.service;
 
-public interface AuctionService {
+import java.util.List;
 
+import com.yedam.app.market.vo.AuctionVO;
+
+public interface AuctionService {
+		// 전체 조회
+		public List<AuctionVO> getAllAuctions();	
+		
+		// 단건 조회
+		public AuctionVO getOneAuction(int ActNo);
+		
+		// 등록
+		public boolean InsertAuction(AuctionVO vo);
+		
+		// 수정
+		public boolean updateAuction(AuctionVO vo);
+		
+		// 삭제
+		public boolean deleteAuction(int ActNo);
 }
