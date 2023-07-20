286b7db38bad76c3c1c5b5e0d89a89ed249b6e43
user <user@DESKTOP-LGGFQD6>
2023-07-20 17:48:57 +0900

    7/20 희영 게시판 시작

---
+++ Farmers/src/main/java/com/yedam/app/board/vo/ReplyVO.java
@@ -0,0 +1,23 @@
+package com.yedam.app.board.vo;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class ReplyVO {
+
+	private String cmtNo;
+	private String memNo;
+	private String cmtDesct;
+	private String boardNo;
+	private int	rcmNum;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(pattern ="yyyy-MM-dd")
+	private Date wrtDate;
+	
+}
