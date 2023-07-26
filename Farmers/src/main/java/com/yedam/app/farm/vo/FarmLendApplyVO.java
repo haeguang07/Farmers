51f4f30b5d0f6d760c0864f357b137368a170f3a
user <user@DESKTOP-9VU4OK8>
2023-07-26 15:54:48 +0900

    0726
    농지대여 파일 다운로드 기능
    
    장바구니 수량 제한
    
    디테일 수정

--- Farmers/src/main/java/com/yedam/app/farm/vo/FarmLendApplyVO.java
+++ Farmers/src/main/java/com/yedam/app/farm/vo/FarmLendApplyVO.java
@@ -1,10 +1,12 @@
 package com.yedam.app.farm.vo;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yedam.app.common.vo.AttachmentVO;
 
 import lombok.Data;
 
@@ -26,6 +28,9 @@ public class FarmLendApplyVO {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	@JsonFormat(pattern ="yyyy-MM-dd")
 	private Date aplDate;
+	private int lendPrice;
+	private String atchFileName;
+	private String uplFileName;
 	
 	//상세코드
 	private String codeDesct;
@@ -40,4 +45,6 @@ public class FarmLendApplyVO {
 	//작성자
 	private String name;
 	private String mbl;
+	
+	private List<AttachmentVO> attachList;
 }
