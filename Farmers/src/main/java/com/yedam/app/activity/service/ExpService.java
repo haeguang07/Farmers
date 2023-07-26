c19a1d0acfa732bc3f59ec8287410a7adcb384e7
pc <pc@DESKTOP-4LRN9VC>
2023-07-27 05:23:07 +0900

    농촌 체험하기 신청VO 생성, 신청 기능 구현 중..

--- Farmers/src/main/java/com/yedam/app/activity/service/ExpService.java
+++ Farmers/src/main/java/com/yedam/app/activity/service/ExpService.java
@@ -2,6 +2,7 @@ package com.yedam.app.activity.service;
 
 import java.util.List;
 
+import com.yedam.app.activity.vo.ExpApplyVO;
 import com.yedam.app.activity.vo.ExpVO;
 
 public interface ExpService {
@@ -15,4 +16,6 @@ public interface ExpService {
 	public int deleteExpInfo(String boardNo);
 	// 게시글 수
 	public int getCount(String expStart, String dst1, String dst2);
+	// 농촌 체험하기 신청(등록)
+	public int insertExpApply(ExpApplyVO expaVO);
 }
